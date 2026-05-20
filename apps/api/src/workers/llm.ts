0f4a8e1aa41c84e2386208009e93059220f307db
unknown <martin659874123@gmail.com>
2026-05-20 22:35:28 +0800

    feat: LLM worker

--- apps/api/src/workers/llm.ts
+++ apps/api/src/workers/llm.ts
@@ -5,39 +5,51 @@ import { type AudiobookRequest } from "@audiobook/shared-libs/schema/audiobook.j
 import { json } from "stream/consumers";
 
 export function startLLMWorker() {
-  boss.work<AudiobookRequest, string>("add-tags", async ([job]) => {
+  boss.work<AudiobookRequest, { bookID: string; ttsUrl: string[]; tags: string[] }>("add-tags", async ([job]) => {
     console.log(`Starting job ${job.id} for llm queue`);
 
     // add to db
     const uuid = crypto.randomUUID();
+    console.log(`Generated UUID ${uuid} for job ${job.id}`);
 
     const data = job.data;
     const text = data.text!;
     // TODO: segment the text into smaller chunks if it's too long, to avoid context length limit of the model:
     // TODO: parse file if passed
+    
     const result = await addTags(text);
+    console.log(`ADD Tags for job ${job.id}: ${result.join(", ")}`);
 
-    const tagId = [];
-    for (const tag of result) {
-      const ttsId = await boss.send("tts", { ...data, text: tag });
-      tagId.push(ttsId!);
-    }
+    const ttsJobIds: (string | null)[] = await Promise.all(
+      result.map((tag) => boss.send("tts", { ...data, text: tag }))
+    );
+    console.log(`Enqueued ${ttsJobIds.length} TTS jobs for llm job ${job.id}`);
+    const ttsUrls: string[] = [];
 
-    const ttsUrl: string[] = [];
-    Promise.all(
-      tagId.map(async (id) => {
-        const [tts] = await boss.findJobs<TTSResponse>("tts", {
-          id: id,
-        });
-        ttsUrl.push(tts?.data.fileUrl!);
-        return tts;
-      }),
-    ).then((res) => {
-      console.log(`All tts jobs done for llm job ${job.id}! Result: ${res}`);
-    });
+    // Polling
+    await Promise.all(
+      ttsJobIds.map(async (id) => {
+        if (!id) return;
+        
+        while (true) {
+          const [ttsJob] = await boss.findJobs<TTSResponse>("tts", { id: id }); 
+          if (ttsJob && ttsJob.state === 'completed') {
+            const fileUrl = ttsJob.data?.fileUrl;
+            if (fileUrl) {
+              ttsUrls.push(fileUrl);
+            }
+            break; 
+          } else if (ttsJob && ttsJob.state === 'failed') {
+            console.error(`TTS Job ${id} failed`);
+            break; 
+          }
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      })
+    );
+    console.log(`All TTS jobs completed for llm job ${job.id}. Collected URLs: ${ttsUrls.join(", ")}`);
+    
 
-    console.log(`Job ${job.id} done in llm queue! Result: ${result}`);
-
-    return json({ uuid, ttsUrl, tags: result });
+    return { bookID: uuid, ttsUrl: ttsUrls, tags: result };
   });
 }
