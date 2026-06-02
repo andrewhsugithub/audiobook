6e9bb9b4ef158152f88f947a116ed94e418670e3
andrewhsugithub <andrew1mail@gmail.com>
2026-06-02 21:51:05 +0800

    update db schema

--- packages/db/src/schema/schema.ts
+++ packages/db/src/schema/schema.ts
@@ -7,6 +7,8 @@ export {
 // export { chapters, chapterPlaylistStatusEnum } from "./chapters.js";
 // export { segmentStatusEnum, segments } from "./segments.js";
 export { segments } from "./segments.js";
-export { users } from "./users.js";
+// export { users } from "./users.js";
 export { providerEnum, voices } from "./voices.js";
 export { assets, assetTypeEnum, uploadStatusEnum } from "./assets.js";
+export { user, session, account, verification } from "./auth.js";
+export { userLibrary } from "./user-library.js";
